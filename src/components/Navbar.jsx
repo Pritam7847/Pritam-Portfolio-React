1fc016f40e4d5dbdd4a362055d6098b5c91c146b
Pritam7847 <himanshusingh7847@gmail.com>
2025-08-03 16:26:21 +0530

    Fixed hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -55,7 +55,7 @@ useEffect(() => {
             <div tabIndex={0} role="button" className="btn btn-ghost bg-transparent">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
+                className="h-5 w-5 bg-inherit"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor">
